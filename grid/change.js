d92e880a5a5bd556131ed3b0c930e6bc62b6696b
Michelle Hernandez <mhernandez317@students.mchenry.edu>
2025-11-10 18:30:40 -0600

    grid

---
+++ grid/change.js
@@ -0,0 +1,15 @@
+function change01() {
+  document.getElementById("large").src = "images/aloe-vera-5157522_640.jpg";
+}
+function change02() {
+  document.getElementById("large").src = "images/brushes-2927793_640.jpg";
+}
+function change03() {
+  document.getElementById("large").src = "images/game-console-6603120_640.jpg";
+}
+function change04() {
+  document.getElementById("large").src = "images/longboard-6185207_640.jpg";
+}
+function change05() {
+  document.getElementById("large").src = "images/old-books-436498_640.jpg";
+}

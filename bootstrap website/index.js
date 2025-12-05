af5885967e6d7fbb348875e7ed96ae2d58cf9a48
Michelle Hernandez <mhernandez317@students.mchenry.edu>
2025-12-05 00:03:02 -0600

    Bootstrap website

---
+++ bootstrap website/index.js
@@ -0,0 +1,12 @@
+function change01() {
+  document.getElementById("large").src = "images/buck-moth.webp";
+}
+function change02() {
+  document.getElementById("large").src = "images/foxglove.jpg";
+}
+function change03() {
+  document.getElementById("large").src = "images/Death-cap-mushroom.webp";
+}
+function change04() {
+  document.getElementById("large").src = "images/black-widow.jpg";
+}
